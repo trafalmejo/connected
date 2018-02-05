e1a6dd5546a4930fd62f510b20c9e04d86bbfa3b
MacOGS <oissadmin@SL0004WKS-MAC.OGS.SA.NYU.EDU>
2018-02-05 11:13:12 -0500

    addButton

--- server.js
+++ server.js
@@ -15,41 +15,68 @@ app.get('/', function (req, res) {
 
 app.post('/process', function (req, res) {
 
-    var name = req.body.username;
 
     var start1 = new Date(req.body.start1);
     var start2 = new Date(req.body.start2);
     var start3 = new Date(req.body.start3);
+    var start4= new Date(req.body.start4);
     var end1 = new Date(req.body.end1);
     var end2 = new Date(req.body.end2);
-    var end3 = new Date(req.body.end3);
+    var end3 = new Date(req.body.end3);    
+    var end4 = new Date(req.body.end4);
+    var range1, range2, range3, range4;
+    var partime1 =  Boolean(req.body.partime1);
+    var partime2 =  Boolean(req.body.partime2);
+    var partime3 =  Boolean(req.body.partime3);
+    var partime4 =  Boolean(req.body.partime4);
+;
 
-	//var days = Math.abs(start1-end1) / (1000*60*60*24);
 
-	// var days = (int) (milliseconds / (1000*60*60*24));
-	if(name == " "){
-		name = "Hey little sunshine";
-	}
 	if(start1.toString() == "Invalid Date" || end1.toString() == "Invalid Date"){
-		start1 = 0;
-		end1 = 0;
+	range1 = 0;
+	}
+	else{
+			range1 = partime1 ? Math.abs(end1 - start1  + 86400000)*.5 : Math.abs(end1 - start1 + 86400000);
+
 	}
 		if(start2.toString() == "Invalid Date" || end2.toString() == "Invalid Date"){
-		start2 = 0;
-		end2 = 0;
+	
+range2 = 0;
+
+	}else{
+			range2 = partime2 ? Math.abs(end2 - start2  + 86400000)*.5 : Math.abs(end2 - start2 + 86400000);
+
 	}
 		if(start3.toString() == "Invalid Date" || end3.toString() == "Invalid Date"){
-		start3 = 0;
-		end3 = 0;
+	
+range3 = 0;
+
+	}
+	else{
+			range3 = partime3 ? Math.abs(end3 - start3  + 86400000)*.5 : Math.abs(end3 - start3 + 86400000);
+
+	}
+if(start4.toString() == "Invalid Date" || end4.oString() == "Invalid Date"){
+range4 = 0;
+
+	}
+	else{
+
+			range4 = partime4 ? Math.abs(end4 - start4  + 86400000)*.5 : Math.abs(end4 - start4 + 86400000);
+
+	}
+	
+
+    var milliseconds = Math.abs(range1+range2+range3+range4);
+    //var milliseconds = Math.abs((end1 - start1))
+	var days = 365 - (milliseconds / (1000*60*60*24));
+	if(days < 0){
+		days = 0 ;
 	}
 
-     var milliseconds = Math.abs((end1 - start1)+(end2 - start2)+(end3 - start3))
-    //var milliseconds = Math.abs((end1 - start1))
-	var days = 365 - Math.floor((milliseconds / (1000*60*60*24)));
 
 
-
-    res.write("<html><body><h1>"+name + ", your have: " + days + " days left to do another OPT"+"</h1></body></html>");
+    res.send("<html><body><h1>"+ "Little Sunshine" + ", your have: " + days + " days left to do another OPT"+"</h1></body></html>");
 
 })
 
